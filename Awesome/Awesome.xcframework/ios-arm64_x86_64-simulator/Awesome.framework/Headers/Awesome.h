f0b8e347e9deb51f835b4ccfc64dbb1ce36b49f9
John Moshakis <john.moshakis@firmex.com>
2021-05-28 22:53:37 -0400

    Added awesome

---
+++ Awesome/Awesome.xcframework/ios-arm64_x86_64-simulator/Awesome.framework/Headers/Awesome.h
@@ -0,0 +1,21 @@
+//
+//  Awesome.h
+//  Awesome
+//
+//  Created by John Moshakis on 5/25/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Awesome.
+FOUNDATION_EXPORT double AwesomeVersionNumber;
+
+//! Project version string for Awesome.
+FOUNDATION_EXPORT const unsigned char AwesomeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Awesome/PublicHeader.h>
+
+#import <Awesome/AwesomeMenuItem.h>
+#import <Awesome/AwesomeMenu.h>
+
+
